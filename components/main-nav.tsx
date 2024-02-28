fb06207021fc3c58c0f13c99ddf537b3e2737d7d
ross-brown <rbrown_98@yahoo.com>
2024-02-27 18:19:36 -0600

    finish store settings page to update and delete store

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,10 +13,15 @@ export default function MainNav({
 
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: "Overview",
+      active: pathname === `/${params.storeId}`
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`
-    }
+    },
   ];
 
   return (
